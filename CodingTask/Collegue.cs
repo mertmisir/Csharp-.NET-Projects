b0a11b847cc6737496c955ea4cb0ba7a1af8183c
Mert Mısır <84075655+mertmisir@users.noreply.github.com>
2022-10-12 13:17:59 +0200

    Colleague Payment Tracking App
     This application has been developed in order to keep track of lunch payments and decide who paid the most. 
     OOP principles has been followed, it basically writes all the details of colleagues to a file and retrieves from a file.

---
+++ CodingTask/Collegue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTask
+{
+    internal class Collegue
+    {
+        private string name;
+        private double foodPrice; 
+        private double money; // to keep track of how much each collegue have!
+
+        //default constructor
+        public Collegue() 
+        {
+            name = "no name";
+            foodPrice = 50; //by default let's our total food price be 50 euros!
+            money = 0;
+        }
+        //constructor
+        public Collegue(string name, double money) 
+        {
+            this.name = name;
+            this.money = money;
+        }
+        //setters & getters
+        public string getName() { return name; }
+        public double getFoodPrice() { return foodPrice; }
+        public double getMoney() { return money; }
+
+        public void setName(string name) { this.name = name; }
+        public void setFoodPrice(double foodPrice) { this.foodPrice = foodPrice; }
+        public void setMoney(double money) { this.money = money; }
+
+    }
+
+   
+}
---
+++ CodingTask/Program.cs
@@ -0,0 +1,69 @@
+﻿using CodingTask;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("======= Welcome to the Collegue Payment App - Mert Misir :) =======");
+        Console.WriteLine("Please press 1 to see your collegues' payment details");
+        Console.WriteLine("Please press any key to quit");
+        Console.Write("What's your choice ? : ");
+
+        string choice;
+        choice = Console.ReadLine();
+        Console.WriteLine();
+
+        if (choice == "1")
+        {
+            Console.WriteLine("OK! Retrieving your collegues details from a file......");
+            InitializeCollegueDetails();
+        }
+        else
+        {
+            Console.WriteLine("Tschüss! :)");
+        }
+    }
+    public static void InitializeCollegueDetails()
+    {
+        try
+        {
+            string path = "C:\\Users\\mertm\\Desktop\\CodingTask\\Collegues.txt";
+            List<string> docs = new List<string>(); // to handle multiple lines of data issue (overwriting multiple lines to a file in run time!) 
+
+            //instantiation of an objects:  
+            Collegue collegue = new Collegue("Mert Misir", 10);
+            Collegue collegue2 = new Collegue("Lebron James", 5);
+            Collegue collegue3 = new Collegue("Leo Messi", 5);
+            Collegue collegue4 = new Collegue("Cristiano Ronaldo", 10);
+            Collegue collegue5 = new Collegue("Dirk Nowitzki", 20);
+
+            docs.Add(collegue.getName() + " " + collegue.getMoney() + "\n" + collegue2.getName() + " " + collegue2.getMoney() + "\n" + collegue3.getName() + " " + collegue3.getMoney() + "\n" + collegue4.getName() + " " + collegue4.getMoney() + "\n" + collegue5.getName() + " " + collegue5.getMoney()); // string concatanation , add the collegue info to the string list so that I can solve overwriting issue!
+            File.WriteAllLines(path, docs); // write collegue info to the file
+
+            string[] tempStorage = File.ReadAllLines(path); //temporary string array to retrieve and display back what we have in the file!
+            foreach (string temp in tempStorage)
+                Console.WriteLine(temp);
+
+            Console.WriteLine();
+            //to find and display who paid the most among collegues!
+            if (collegue.getMoney() > collegue2.getMoney() && collegue.getMoney() > collegue3.getMoney() && collegue.getMoney() > collegue4.getMoney() && collegue.getMoney() > collegue5.getMoney())
+                Console.WriteLine(collegue.getName() + " has paid the most!");
+
+            else if (collegue2.getMoney() > collegue.getMoney() && collegue2.getMoney() > collegue3.getMoney() && collegue2.getMoney() > collegue4.getMoney() && collegue2.getMoney() > collegue5.getMoney())
+                Console.WriteLine(collegue2.getName() + " has paid the most!");
+
+            else if (collegue3.getMoney() > collegue.getMoney() && collegue3.getMoney() > collegue2.getMoney() && collegue3.getMoney() > collegue4.getMoney() && collegue3.getMoney() > collegue5.getMoney())
+                Console.WriteLine(collegue3.getName() + " has paid the most!");
+
+            else if (collegue4.getMoney() > collegue.getMoney() && collegue4.getMoney() > collegue2.getMoney() && collegue4.getMoney() > collegue3.getMoney() && collegue4.getMoney() > collegue5.getMoney())
+                Console.WriteLine(collegue4.getName() + " has paid the most!");
+
+            else
+                Console.WriteLine(collegue5.getName() + " has paid the most!");
+
+
+            Console.ReadKey(true);
+        }
+        catch (Exception e) { Console.WriteLine(e.Message); }
+    }
+}
